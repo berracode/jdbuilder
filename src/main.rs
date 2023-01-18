ad71851694dd3f464d06be96f7e9f7b4f2b37524
QuantumInformatik <juanguillermoalarcon@gmail.com>
2023-01-17 23:38:21 -0500

    do_something as handle is added

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 
 mod command;
 
-use command::RustflixArgs;
+use command::{RustflixArgs, EntityType, EntityType::User, EntityType::Video};
 use clap::Parser;
 
 fn main() {
    let args = RustflixArgs::parse();
+   let entity_type = args.entity_type;
+
+   match entity_type {
+      User(user) => do_something(),
+      Video(video) => do_something()
+   }
+}
+
+fn do_something(){
+   println!("Doing something important...");
 }
