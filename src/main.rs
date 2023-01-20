6209b0ec3e726e35c6bdea4bb1e92d074981df52
QuantumInformatik <juanguillermoalarcon@gmail.com>
2023-01-19 22:10:45 -0500

    additional methods for commadns and subcommands have been added

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,55 @@
 
 mod command;
 
-use command::{RustflixArgs, EntityType, EntityType::User, EntityType::Video};
+use command::{
+   RustflixArgs, 
+   EntityType::User, 
+   EntityType::Video, 
+   user_command::{
+      UserCommand, 
+      CreateUser
+   }, 
+   video_command::{
+      VideoCommand,
+      CreateVideo, UpdateVideo 
+   }
+};
 use clap::Parser;
 
+use crate::command::video_command::VideoSubcommand;
+
 fn main() {
+
    let args = RustflixArgs::parse();
    let entity_type = args.entity_type;
 
    match entity_type {
-      User(user) => do_something(),
-      Video(video) => do_something()
+      User(user) => handle_user_command(user),
+      Video(video) => handle_video_command(video)
    }
 }
 
-fn do_something(){
+fn handle_user_command(user: UserCommand){
    println!("Doing something important...");
+}
+
+fn handle_video_command(video: VideoCommand){
+   println!("Doing something important... {:?}",video);
+
+   match video.command {
+      VideoSubcommand::Create(createVideo) => create_video(createVideo),
+      VideoSubcommand::Update(updateVideo) => update_video(updateVideo),
+   }
+
+}
+
+fn create_video(video: CreateVideo){
+   println!("Creating video... ");
+   //TODO write in file the video info
+}
+
+fn update_video(video: UpdateVideo){
+   println!("Updating video... ");
+   //TODO update in file the video info
+   
 }
